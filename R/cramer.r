a7b0e4ba97e3c0cd118e908dae3eee5a37bb77f8
CCCelestine <celestine.chareyre@orange.fr>
2020-12-06 13:58:39 +0100

    FONCTION V CRAMER

---
+++ R/cramer.r
@@ -0,0 +1,20 @@
+#V de Cramer
+#x = variable qualitative
+#y = variable classe d'appartenance
+#' Title
+#'
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
+vcramer<-function(x,y){
+  #tableau de contingence
+  tableau=table(x,y)
+  #V de Cramer
+  #0 (absence de liaison) et 1 (liaison parfaite)
+  res=cramer.v(tableau)
+  return(res)
+}
