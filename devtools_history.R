303067fdca3d7039bb2ae804c1afef9ee52965da
CCCelestine <celestine.chareyre@orange.fr>
2020-12-07 11:15:45 +0100

    ajout de df_test

--- devtools_history.R
+++ devtools_history.R
@@ -4,4 +4,15 @@ usethis::use_package('questionr')
 usethis::use_package('ggplot2')
 usethis::use_package('ggpubr')
 usethis::use_package('dplyr')
+usethis::use_package('readxl')
 usethis::use_build_ignore("devtools_history.R")
+
+df_test<-read_xlsx("D:/Documents/M2 SISE/Programmation R/Projet/OneDrive_1_07-12-2020/df_test.xlsx")
+setwd("D:/Documents/M2 SISE/Programmation R/Projet/version_finale/siseanalytics/")
+usethis::use_data(df_test)
+
+mydir <- "D:/Documents/M2 SISE/Programmation R/Projet/version_finale/"
+mypackage <- "siseanalytics"
+path <- file.path(mydir, mypackage)
+setwd(path)
+document()
