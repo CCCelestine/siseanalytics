0e4b77fa306808f89eca3bc44d409f2516cb612d
CCCelestine <celestine.chareyre@orange.fr>
2020-12-07 11:58:54 +0100

    suppression de fromage + ajout de documentation pour df_test

---
+++ R/df_test.R
@@ -0,0 +1,11 @@
+#' Dataset df_test
+#'
+#' A dataset containing the test data
+#'
+#' @format A data frame with 357 rows and 8 variables:
+#' \describe{
+#'   \item{val_pred}{val_pred}
+#'   \item{val_reel}{val_reel}
+#'   ...
+#' }
+"df_test"
--- devtools_history.R
+++ devtools_history.R
@@ -10,12 +10,4 @@ usethis::use_build_ignore("devtools_history.R")
 df_test<-read_xlsx("D:/Documents/M2 SISE/Programmation R/Projet/OneDrive_1_07-12-2020/df_test.xlsx")
 setwd("D:/Documents/M2 SISE/Programmation R/Projet/version_finale/siseanalytics/")
 usethis::use_data(df_test)
-fromage=read.table("D:/Documents/M2 SISE/Programmation R/Projet/fromage.txt",header=T,row.names = 1,sep = "\t",dec=".")
-setwd("D:/Documents/M2 SISE/Programmation R/Projet/version_finale/siseanalytics/")
-usethis::use_data(fromage)
 
-mydir <- "D:/Documents/M2 SISE/Programmation R/Projet/version_finale/"
-mypackage <- "siseanalytics"
-path <- file.path(mydir, mypackage)
-setwd(path)
-document()
