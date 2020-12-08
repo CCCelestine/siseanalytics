4cd4986cc1495d0d2755caa1e96bcb87e08792f8
A-Iborra <alex.iborra@orange.fr>
2020-12-08 11:38:49 +0100

    Modif commentaire + test exemple

--- R/pred_reel_2c.R
+++ R/pred_reel_2c.R
@@ -4,7 +4,7 @@
 #'
 #' @format A data frame with 357 rows and 2 variables:
 #' \describe{
-#'   \item val_pred, predicted value
-#'   \item val_reel, real values
+#'   \item val_pred,predicted  membership class variable
+#'   \item val_reel, real membership class variable
 #' }
 "pred_reel_2c"
