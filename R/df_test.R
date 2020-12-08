d573301e9bea418cd25d84bb170778e45881f2fb
A-Iborra <alex.iborra@orange.fr>
2020-12-08 12:29:46 +0100

    Modif description data

--- R/df_test.R
+++ R/df_test.R
@@ -4,13 +4,13 @@
 #'
 #' @format A data frame with 357 rows and 8 variables:
 #' \describe{
-#'   \item val_pred,predicted  membership class variable
-#'   \item val_reel, real membership class variable
-#'   \item taille, the height of the person
-#'   \item poids, the person's weight in kg
-#'   \item pointure, The shoe size
-#'   \item sexe, Man or woman
-#'   \item sport,sport practiced by the individual
-#'   \item val_parent, parent target value
+#'   \item {val_pred} {predicted  membership class variable}
+#'   \item {val_reel} {real membership class variable}
+#'   \item {taille} {the height of the person}
+#'   \item {poids} {the person's weight in kg}
+#'   \item {pointure} {The shoe size}
+#'   \item {sexe} {Man or woman}
+#'   \item {sportsport} {practiced by the individual}
+#'   \item {val_parent} {parent target value}
 #' }
 "df_test"
